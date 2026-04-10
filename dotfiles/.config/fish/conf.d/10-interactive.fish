0fa7f270fa88738607dfda2417018625cce80997
pratyaksh52 <choudharypratyakshpc@gmail.com>
2026-04-11 00:53:42 +0530

    Use --shims for non-interactive mise activation
    Update fish startup script to conditionally activate mise: when the shell is interactive, run `mise activate fish | source` as before; when non-interactive, run `mise activate fish --shims | source` to avoid loading interactive shims. This ensures proper behavior for non-interactive sessions.

--- dotfiles/.config/fish/conf.d/10-interactive.fish
+++ dotfiles/.config/fish/conf.d/10-interactive.fish
@@ -24,5 +24,9 @@ if type -q uv
 end
 
 if command -q mise
-    mise activate fish | source
+    if status is-interactive
+        mise activate fish | source
+    else
+        mise activate fish --shims | source
+    end
 end
